a626f8d4d6c13a083069f08cc0377298a0a8424e
Yuno-gsai <sr.bonilla17@gmail.com>
2025-06-07 23:23:12 -0600

    Primeros cambios

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const app = express();
 // ─── 0) MIDDLEWARES GENERALES (incluyendo CORS) ────────────────────────
 app.use(
   cors({
-    origin: "http://localhost:5173", // o el host/puerto de tu frontend
+    origin: [
+      "http://localhost:5173", // Para el desarrollo local
+      "https://chatparaproyectodedaw.azurewebsites.net" // Para el entorno en Azure
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -54,7 +57,10 @@ app.post("/", (req, res) => {
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: [
+      "http://localhost:5173", // Para el desarrollo local
+      "https://chatparaproyectodedaw.azurewebsites.net" // Para el entorno en Azure
+    ],
     methods: ["GET", "POST"],
   },
 });
